6298f279127f983fa73d8951390e7bceb9d24071
richard <2bloveys@gmail.com>
2022-09-28 15:22:25 +0800

    Initial Commit

---
+++ ios_hw1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_hw1
+//
+//  Created by RICHARD on 2022/9/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_hw1/ios_hw1App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_hw1App.swift
+//  ios_hw1
+//
+//  Created by RICHARD on 2022/9/28.
+//
+
+import SwiftUI
+
+@main
+struct ios_hw1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
